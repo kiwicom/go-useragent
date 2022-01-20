d953bfe379d574ae75e0f9b6840bfc457582b911
Ivan Boyarkin <19638942+mrVanboy@users.noreply.github.com>
2022-01-20 09:28:46 +0100

    feat: Add methods for one-line errorless string generation
    This commits:
    - adds implementation of the fmt.Stringer interface to simplify passing
    to the functions from the fmt package.
    - extracts and exports method for validation.
    - adds MustFormat method as an alternative for the initialization of
    global variables or using in tests.

--- ua.go
+++ ua.go
@@ -1,5 +1,7 @@
 package useragent
 
+import "fmt"
+
 // UserAgent contains all available fields in UserAgent according to RFC-22 rules.
 type UserAgent struct {
 	// Name is the service name
@@ -10,4 +12,23 @@ type UserAgent struct {
 	Environment string
 	// SystemInfo describes extra metadata (e.g. node-fetch 1.2)
 	SystemInfo string
+}
+
+// Validate an ua struct values and ensure that Format call generate a valid
+// useragent. Descriptive non-nil error will be returned if any field of the ua
+// contains value that prevents generation of correct RFC 22 useragent.
+func (ua UserAgent) Validate() error {
+	appNameMatch := createInputRegex.MatchString(ua.Name)
+	if !appNameMatch {
+		return fmt.Errorf("error validating %q: %w", ua.Name, ErrNameInvalid)
+	}
+	versionMatch := createInputRegex.MatchString(ua.Version)
+	if !versionMatch {
+		return fmt.Errorf("error validating %q: %w", ua.Version, ErrVersionInvalid)
+	}
+	environmentMatch := createInputRegex.MatchString(ua.Environment)
+	if !environmentMatch {
+		return fmt.Errorf("error validating %q: %w", ua.Environment, ErrEnvironmentInvalid)
+	}
+	return nil
 }
